c059c3d60bfbd4e01707f811f54d8dce4e91d456
Raji Gbenga Kabir <rajigbenga5@gmail.com>
2023-05-17 06:34:30 -0700

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,42 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    """
-    Converts a Roman numeral to an integer.
-    ...
+    table = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
-    Parameters
-    ----------
-    roman_string : str
-        the roman numeral to convert
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    prev, sum = 0, 0
 
-    Return:
-        0 If the roman_string is not a string or None
-        The corresponding integer otherwise.
-    """
-
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
-    num = 0
-
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-        else:
-            num += roman_dict[roman_string[i]]
-
-    return (num)
+    for c in roman_string:
+        sum += table[c] if table[c] <= prev else table[c] - prev * 2
+        prev = table[c]
+    return sum
